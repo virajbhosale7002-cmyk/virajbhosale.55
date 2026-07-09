febd55a67b701553c2d30806730895a503315eae
Unit 1 <virajbhosale7002@gmail.com>
2026-07-09 10:06:30 +0530

    Add files via upload

---
+++ large.py
@@ -0,0 +1,13 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+
+if a >= b and a>= c:
+    print("Largest number is:",a)
+elif b >= a and b>= c:
+    print("Largest number is:",b)
+else:
+    print("largest number is:",c)
+
+          
+
