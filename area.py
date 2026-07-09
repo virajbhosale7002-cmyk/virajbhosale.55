22800ccb2042763fcfd2389958eb64cb33f2de0f
Unit 1 <virajbhosale7002@gmail.com>
2026-07-09 09:48:13 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,6 @@
+length =float(input("Enter length: "))
+breadth =float(input("Enter breadth: "))
+
+area = length*breadth
+
+print("Area of rectangle =",area)
---
+++ swap.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+a,b=b,a
+
+print("After swappin:")
+print("a=",a)
+print("b=",b)
